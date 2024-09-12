aaa98c4c9b7b9c00c5221c6649ec5c5af7299be8
Akkadev2219 <akashdhuriya2219@gmail.com>
2024-09-12 23:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function daysOfAYear(year) {
         if (year % 100 === 0) {
             if (year % 400 === 0) {
                 return 366;
+            } else {
+                return 365;
+            }
         } else {
-            return 365;
-        }
+            return 366;
+        } 
     } else {
-        return 366;
-    } 
-	else {
-		return 365;
-	}
+        return 365;
+    }
 }
 console.log(daysOfAYear(2022));
 console.log(daysOfAYear(2024));
